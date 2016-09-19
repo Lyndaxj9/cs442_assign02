0ca74a5db6853d477ac3d362f1276c710c413114
Thomas Mathew <tmathew4@cs.binghamton.edu>
2016-09-19 14:27:11 -0400

    added FileProcessor and Student classes

---
+++ registrationScheduler/src/registrationScheduler/FileProcessor.java
@@ -0,0 +1,50 @@
+import java.io.BufferedReader;
+import java.io.File;
+import java.io.FileReader;
+import java.io.IOException;
+
+public class FileProcessor {
+	private String line;
+	private File file;
+	private FileReader filereader;
+	private BufferedReader bufferedReader;
+	private String lines[] = new String [80];
+	
+	//Constructor
+	public FileProcessor(){
+		
+		try{
+			file = new File("input.txt");
+			filereader = new FileReader(file);
+			bufferedReader = new BufferedReader (filereader);
+			storeLines();
+			
+		} catch (IOException e) {
+			e.printStackTrace();
+		
+		}
+		
+	
+	}
+
+	public void storeLines() throws IOException {
+		int numLines = 0;
+		
+		try {
+			while((line = bufferedReader.readLine()) !=null){
+					lines[numLines] = line;
+					numLines++;
+			}
+		} catch (IOException e) {
+			e.printStackTrace();
+		}finally{
+			filereader.close();
+		}
+
+	}
+	
+	public String getLine(int i){
+		return lines[i];
+	}
+			
+}	
---
+++ registrationScheduler/src/registrationScheduler/Student.java
@@ -0,0 +1,52 @@
+
+public class Student {
+
+	//An array to store four preferences of a student
+	private int[] preference = new int [7];
+	
+	//An array to store the final schedule of a student
+	private int[] schedule = new int [7];
+	
+	//The name of the student
+	private String name;
+	
+	//Validates whether a student can have a valid schedule
+	private boolean isValidSchedule = false;
+	
+	
+	
+	public int[] getPreference() {
+		return preference;
+	}
+
+	public void setPreference(int[] preferenceIn) {
+		for(int i = 0;i < 7;i++)
+			this.preference[i] = preferenceIn[i];
+	}
+
+	public int[] getSchedule() {
+		return schedule;
+	}
+
+	public void setSchedule(int[] scheduleIn) {
+		for(int i = 0;i < 7;i++)
+			this.schedule[i] = scheduleIn[i];
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String nameIn) {
+		this.name = nameIn;
+	}
+
+	public boolean isScheduleCheck() {
+		return isValidSchedule;
+	}
+
+	public void setScheduleCheck(boolean scheduleCheckIn) {
+		this.isValidSchedule = scheduleCheckIn;
+	}
+	
+}	
