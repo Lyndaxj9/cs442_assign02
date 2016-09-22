7e98a760bd022168edd292e42f91bda7bfcde1e8
Thomas Mathew <tmathew4@cs.binghamton.edu>
2016-09-22 16:54:47 -0400

    added code to run

--- registrationScheduler/src/registrationScheduler/threadMgmt/WorkerThread.java
+++ registrationScheduler/src/registrationScheduler/threadMgmt/WorkerThread.java
@@ -1,12 +1,49 @@
-
 package registrationScheduler.threadMgmt;
+
+import registrationScheduler.util.Logger;
+import registrationScheduler.util.FileProcessor;
+import registrationScheduler.store.StdoutDisplayInterface;
 
 public class WorkerThread implements Runnable  {
 
-    // Should we have members here to take in the parameter in
+    //have data members forc
+    FileProcessor filePro;
+    StdoutDisplayInterface results;
+    private static Logger log;
+    // Students
+    // Results
+    //
+
     // createWorkers
+    public WorkerThread(FileProcessor i_filePro, StdoutDisplayInterface i_results, Logger i_log){
+        this.filePro = i_filePro;
+        this.results = i_results;
+        this.log = i_log;
+        log.writeMessage("CONSTRUCTOR: WorkerThread() called.", Logger.DebugLevel.CONSTRUCTOR);
+    }
 
     public void run() {
+        log.writeMessage("METHOD: run() called.", Logger.DebugLevel.THREAD_RUN);
+	//FileProcessor fileprocessor = new FileProcessor();
+		int countLines = 0;
+		String line;
+		String delim = "\\s+";
+		Student studentArray[] = new Student[80];
+		String [] parsedString = new String [8];
+		int [] prefArray = new int [7];
+		Student aStudent;
+
+		while( (line = filePro.getLine(countLines)) != null){
+			parsedString = line.split(delim);
+			aStudent = new Student();
+			aStudent.setName(parsedString[0]);
+			for(int i = 1;i<8;i++){
+				prefArray[i-1] = Integer.parseInt(parsedString[i]);
+			}
+			aStudent.setPreference(prefArray);
+			studentArray[countLines] = aStudent;
+			countLines++;
+		}
 	// ...
     // Read lines from file using fileProcessor
     // Create all course objects in ObjectPool
@@ -17,6 +54,11 @@ public class WorkerThread implements Runnable  {
     // method{Check if top class has available seats and if so assign the seat, using ObjectPool}
     // method{Store results at everystep or at the end into Results class
     }
+
+    //read from file and store preferences in student
+
+    // - ALGO METHODS
+    //get
 
     
 
